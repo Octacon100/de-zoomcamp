441dcfb20c754e049cffc37dfdcf8d9ade58d04d
Octacon100 <Octacon100@gmail.com>
2023-02-06 11:13:29 -0500

    File renames

--- Week2/etl_gcs_to_bq_deployment.py
+++ Week2/etl_gcs_to_bq_deployment.py
@@ -1,14 +1,15 @@
-from etl_gcs_to_bg import etl_gcs_to_bg
+import etl_gcs_to_bq
 from prefect.deployments import Deployment
 from prefect.filesystems import GitHub
 
 github_block = GitHub.load("github-de-zoomcourse-code")
 
 deployment = Deployment.build_from_flow(
-    flow=etl_gcs_to_bg,
+    flow=etl_gcs_to_bq.etl_gcs_to_bq,
     name="de_course_bq_upload",
     version="1",
     tags=["demo"],
     storage=github_block,
+    path="github_deployed_code"
 )
 deployment.apply()
